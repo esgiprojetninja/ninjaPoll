4d3861a7db8e6b13c837300d6c3c0343d1d60a40
Roland Kullet <renaud.bellec.3@gmail.com>
2017-03-23 14:12:25 +0100

    answer polls

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :answers
+  post 'answers/save-all', to: 'answers#save_all', as: :answer_poll
   resources :poll_questions
   root 'home#index'
   resources :poll_questions
