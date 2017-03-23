f2844201d06db5fa66c1cb447a67e8a4d79a7f2c
Roland Kullet <renaud.bellec.3@gmail.com>
2017-03-23 12:13:24 +0100

    added method, template and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :answers
   resources :poll_questions
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
   resources :poll_questions
   resources :polls
+  get 'polls/:id/answer', to: 'polls#answer', as: :poll_answer
 end
