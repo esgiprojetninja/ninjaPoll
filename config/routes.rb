ecfcabd2be8f395f178d584d6a3e43dbd1374472
Renaud Bellec <roland@MacBook-Pro-de-Renaud.local>
2017-04-11 16:17:24 +0200

    wip

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :answers
-  resources :users
   resources :poll_questions
   resources :polls
   post 'answers/save-all', to: 'answers#save_all', as: :answer_poll
   root 'home#index'
   get 'polls/:id/answer', to: 'polls#answer', as: :poll_answer
+
+  # User
+  resources :users
+  get 'signup', to: 'users#new', as: :signup
 end
