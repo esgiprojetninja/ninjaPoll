0434049c01b06443d7e1f9ffb338252901995f37
Roland Kullet <renaud.bellec.3@gmail.com>
2017-04-11 18:07:36 +0200

    one more test

--- test/integration/user_signup_test.rb
+++ test/integration/user_signup_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class UserSignupTest < ActionDispatch::IntegrationTest
-  test "Invalid signup information" do
+
+  test "invalid signup information" do
     get signup_path
     assert_no_difference "User.count" do
       post users_path, params: { user: {
@@ -12,4 +13,16 @@ class UserSignupTest < ActionDispatch::IntegrationTest
         }}
       end
     end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference "User.count", 1 do
+      post users_path, params: { user: {
+          name: "Denis Richie",
+          email: "awesome@me.io",
+          password: "Password",
+          password_confirmation: "Password"
+        }}
+    end
+  end
 end
