dbf1dfbad9d5fd5a94c946c204323c567bad3786
tmeksava <teddy.meksavanh@arkena.com>
2017-04-11 19:07:03 +0200

    Add types routes & crud

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :types
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :answers
   post 'answers/save-all', to: 'answers#save_all', as: :answer_poll
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170411142845) do
+ActiveRecord::Schema.define(version: 20170411165520) do
 
   create_table "answers", force: :cascade do |t|
     t.integer  "value"
@@ -34,9 +34,16 @@ ActiveRecord::Schema.define(version: 20170411142845) do
     t.string   "name"
     t.datetime "pub_date"
     t.text     "description"
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.boolean  "active",      default: true
+  end
+
+  create_table "types", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.boolean  "active",  default: true
   end
 
   create_table "users", force: :cascade do |t|
