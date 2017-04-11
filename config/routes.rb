10203385165c4c7d252061b5f90203928e8e2f16
Roland Kullet <renaud.bellec.3@gmail.com>
2017-04-11 12:51:23 +0200

    scaffolded user

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :answers
-  post 'answers/save-all', to: 'answers#save_all', as: :answer_poll
-  resources :poll_questions
-  root 'home#index'
+  resources :users
   resources :poll_questions
   resources :polls
+  post 'answers/save-all', to: 'answers#save_all', as: :answer_poll
+  root 'home#index'
   get 'polls/:id/answer', to: 'polls#answer', as: :poll_answer
 end
