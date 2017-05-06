d04139a0045737b343611edf3e27f744994b69f4
dydyFos <dylanfoster.esgi@gmail.com>
2017-05-06 19:04:45 +0200

    cookies functionnal

--- app/helpers/session_helper.rb
+++ app/helpers/session_helper.rb
@@ -5,13 +5,22 @@ module SessionHelper
   end
 
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     session.delete(:is_admin)
     @current_user = nil
   end
 
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
   end
 
   def logged_in?
@@ -27,4 +36,17 @@ module SessionHelper
   def is_admin?
     !session[:is_admin]
   end
+
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
 end
