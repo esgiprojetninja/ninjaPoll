f7228c61c3f02bf49ade24e5edc4151bbd39efd3
dydyFos <dylanfoster.esgi@gmail.com>
2017-06-27 16:44:08 +0200

    removed comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,28 +11,23 @@ class User < ApplicationRecord
   )
   has_secure_password
   validates :password, presence: true , length: {minimum: 6}
-  # Defining an attribute to current user
   attr_accessor :remember_token
 
-  # Returns the hash digest of the given string.
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ?
       BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
 
-  # Returns a random token.
   def User.new_token
     SecureRandom.urlsafe_base64
   end
 
   def remember
-    # points to current user's attribute, not a local variable
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
   end
 
-  # Returns whether the token matches the digest or not
   def authenticated?(remember_token)
     return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
